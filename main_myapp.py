330600f3cdb56009e6a235cd057b6c6a6e64bdd8
aliyakalykulova <110055731+aliyakalykulova@users.noreply.github.com>
2025-09-03 16:09:57 +0500

    Update main_myapp.py

--- main_myapp.py
+++ main_myapp.py
@@ -81,11 +81,13 @@ if gender != st.session_state.select_gender:
 if st.session_state.select_gender == gender_types[1]:
     col1, col2 = st.columns([1, 20])  # col2 будет посередине
     with col2:
-        reproductive_status = st.selectbox( "Репродуктивный статус", rep_status_types)
-        if reproductive_status != st.session_state.select_reproductive_status:
-            st.session_state.select_reproductive_status = reproductive_status
-           # st.session_state.show_result_1 = False
-            st.session_state.show_result_2 = False
+        reproductive_status = st.selectbox( "Репродуктивный статус", rep_status_types, key="select_reproductive_status")
+
+
+       if reproductive_status != st.session_state.select_reproductive_status:
+              st.session_state.select_reproductive_status = reproductive_status
+              st.session_state.show_result_1 = False
+              st.session_state.show_result_2 = False
           
 if st.session_state.select_reproductive_status==rep_status_types[1]:
   col1, col2 = st.columns([3, 20])  # col2 будет посередине
