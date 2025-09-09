2a4f779f44301b0611f8257d3dea0256bb0c421b
aliyakalykulova <110055731+aliyakalykulova@users.noreply.github.com>
2025-09-09 12:08:50 +0500

    Update main_myapp.py

--- main_myapp.py
+++ main_myapp.py
@@ -844,12 +844,13 @@ if user_breed:
                                               if nutr_text[0] in other_for_adult:
                                                 norma = other_for_adult[nutr_text[0]]*(st.session_state.weight_sel**0.75)
                                                 st.pyplot(bar_print(norma, count_nutr_cont_all.get(nutris, ''), nutr_text[0]+", "+ emg, str(emg)))
-                                
-                                  emg = "g"
-                                  nutr_text="ЭПК (50-60%) + ДГК (40-50%)"
-                                  norma = other_for_adult[nutr_text]*(st.session_state.weight_sel**0.75)
-                                  nutris=count_nutr_cont_all.get(other_nutrients_3[0], '')+count_nutr_cont_all.get(other_nutrients_3[1], '')
-                                  st.pyplot(bar_print(norma, nutris, nutr_text+", "+ emg, str(emg)))
+                                  coli, colii=st.columns([6,3])
+                                  with coli:
+                                      emg = "g"
+                                      nutr_text="ЭПК (50-60%) + ДГК (40-50%)"
+                                      norma = other_for_adult[nutr_text]*(st.session_state.weight_sel**0.75)
+                                      nutris=count_nutr_cont_all.get(other_nutrients_3[0], '')*0.5+count_nutr_cont_all.get(other_nutrients_3[1], '')*0.5
+                                      st.pyplot(bar_print(norma, nutris, nutr_text+", "+ emg, str(emg)))
                                 
                                   st.markdown("#### 🪨 Минералы")
                                   coli, colii=st.columns([6,3])
